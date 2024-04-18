b74ab0bb88242e78b27c4c31e97602880cdc89d9
Thiago Puntar <thiagopuntar@gmail.com>
2024-04-18 19:31:53 -0300

    signup backend route

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,3 +1,6 @@
+const Joi = require('joi');
+const bcrypt = require('bcrypt');
+
 const User = require('../models/user');
 
 module.exports.getUsers = async (req, res) => {
@@ -5,8 +8,26 @@ module.exports.getUsers = async (req, res) => {
   res.json(users);
 };
 
-module.exports.saveUser = (req, res, next) => {
-  User.create(req.body)
+module.exports.saveUser = async (req, res) => {
+  const schema = Joi.object({
+    email: Joi.string().email().required(),
+    password: Joi.string().required(),
+    name: Joi.string().optional().min(2).max(30),
+    avatar: Joi.string().optional().uri(),
+    job: Joi.string().optional().min(2).max(30),
+  });
+
+  const { error } = schema.validate(req.body);
+  if (error) {
+    return res.status(400).json(error.details);
+  }
+
+  const { password, ...data } = req.body;
+
+  const salt = await bcrypt.genSalt();
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  return User.create({ ...data, password: hashedPassword })
     .then((user) => {
       res.json(user);
     })
@@ -14,3 +35,10 @@ module.exports.saveUser = (req, res, next) => {
       res.status(400).json(err);
     });
 };
+
+// module.exports.login = (req, res) => {
+//   const { email, password } = req.body;
+//   User.find({ email }).then((user) => {
+//
+//   });
+// }
--- backend/models/user.js
+++ backend/models/user.js
@@ -1,11 +1,21 @@
+const validator = require('validator');
 const mongoose = require('mongoose');
 
 const { Schema, model } = mongoose;
 
+const validateEmail = (email) => validator.isEmail(email);
+
 const userSchema = new Schema({
-  name: { type: String, required: true, minlength: 2, maxlength: 30 },
-  about: { type: String, required: true, minlength: 2, maxlength: 30 },
-  avatar: { type: String, required: true },
+  name: { type: String, minlength: 2, maxlength: 30, default: "Jacques Cousteau" },
+  about: { type: String, minlength: 2, maxlength: 30 },
+  avatar: { type: String },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: [validateEmail, 'Email is invalid'],
+  },
+  password: { type: String, required: true, select: false },
 });
 
 module.exports = model('User', userSchema);
